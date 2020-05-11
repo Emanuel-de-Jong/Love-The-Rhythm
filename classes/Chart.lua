e7af38794805d2293af51f8d69ddd5041d5efb61
Manuel de Jong <manuel@jaarsma-deboer.nl>
2020-05-11 18:40:10 +0200

    read and tableify osu files

--- classes/Chart.lua
+++ classes/Chart.lua
@@ -3,6 +3,26 @@ local Chart = Class:new()
 Chart.path = ""
 Chart.name = ""
 
+Chart.Format = ""
+Chart.General = {}
+Chart.Editor = {}
+Chart.Metadata = {}
+Chart.Difficulty = {}
+Chart.Events = {}
+Chart.TimingPoints = {}
+Chart.HitObjects = {}
+
+local typePerCategory = {
+    Format = "format",
+    General = "setting",
+    Editor = "setting",
+    Metadata = "setting",
+    Difficulty = "setting",
+    Events = "event",
+    TimingPoints = "event",
+    HitObjects = "event",
+}
+
 Chart.construct = function(self, name, path)
     self.name = name
     self.path = path
@@ -10,6 +30,30 @@ Chart.construct = function(self, name, path)
 end
 
 Chart.load = function(self)
+    self:getFileInfo()
+end
+
+Chart.getFileInfo = function(self)
+    local category = "Format"
+    local type = "format"
+    local colonPos = 0
+    for line in io.lines(self.path) do
+        if #line ~= 0 and string.sub(line, 1, 2) ~= "//" then
+            if string.sub(line, 1, 1) == "[" then
+                category = string.sub(line, 2, #line - 1)
+                type = typePerCategory[category]
+            else
+                if type == "format" then
+                    self.Format = string.sub(line, #line - 1)
+                elseif type == "setting" then
+                    colonPos = string.find(line, ":")
+                    self[category][string.sub(line, 1, colonPos - 1)] = string.sub(line, colonPos + 1)
+                elseif type == "event" then
+                    table.insert(self[category], line)
+                end
+            end
+        end
+    end
 end
 
 return Chart
