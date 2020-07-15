bef6a2ea20c9246905a115ff218d73fac93aad58
Manuel de Jong <manuel@jaarsma-deboer.nl>
2020-07-15 12:34:57 +0200

    changed default window size to 1600x900. made main love callbacks dynamic. added Halvetica font. Improved TestScene.

--- main.lua
+++ main.lua
@@ -7,50 +7,56 @@ Class = require("classes/Class")
 local InputManager = require("instances/InputManager")
 local SceneManager = require("instances/SceneManager")
 
+local callbacks = {
+	"directorydropped",
+	"displayrotated",
+	"draw",
+	"errhand",
+	"errorhandler",
+	"filedropped",
+	"focus",
+	"gamepadaxis",
+	"gamepadpressed",
+	"gamepadreleased",
+	"joystickadded",
+	"joystickaxis",
+	"joystickhat",
+	"joystickpressed",
+	"joystickreleased",
+	"joystickremoved",
+	"keypressed",
+	"keyreleased",
+	"lowmemory",
+	"mousefocus",
+	"mousemoved",
+	"mousepressed",
+	"mousereleased",
+	"quit",
+	"resize",
+	"textedited",
+	"textinput",
+	"threaderror",
+	"touchmoved",
+	"touchpressed",
+	"touchreleased",
+	"update",
+	"visible",
+	"wheelmoved",
+}
+
 function love.load(arg, unfilteredArg)
     -- SceneManager.set("Test")
     SceneManager.set("Menu")
 end
 
--- goes through all used love callback functions and calls the corresponding function of the current scene
-function love.update(dt)
-    if SceneManager.scene.update then
-        SceneManager.scene.update(dt)
-    end
-end
-
-function love.draw()
-    if SceneManager.scene.draw then
-        SceneManager.scene.draw()
-    end
-end
-
-function love.mousepressed(x, y, button, istouch, presses)
-    InputManager.mousepressed(x, y, button, istouch, presses)
-
-    if SceneManager.scene.mousepressed then
-        SceneManager.scene.mousepressed(x, y, button, istouch, presses)
-    end
-end
-
-function love.keypressed(key, scancode, isrepeat)
-    InputManager.keypressed(key, scancode, isrepeat)
-
-    if SceneManager.scene.keypressed then
-        SceneManager.scene.keypressed(key, scancode, isrepeat)
-    end
-end
-
-function love.wheelmoved(x, y)
-    InputManager.wheelmoved(x, y)
-
-    if SceneManager.scene.wheelmoved then
-        SceneManager.scene.wheelmoved(x, y)
-    end
-end
-
-function love.resize(w, h)
-    if SceneManager.scene.resize then
-        SceneManager.scene.resize(w, h)
+-- goes through all love callback functions and calls the corresponding function of the current scene
+for _, callback in pairs(callbacks) do
+    love[callback] = function(...)
+        if InputManager[callback] then
+            InputManager[callback](...)
+        end
+        if SceneManager.scene[callback] then
+            SceneManager.scene[callback](...)
+        end
     end
 end
--- scenes/TestScene.lua
+++ scenes/TestScene.lua
@@ -5,23 +5,149 @@ testing stuff.
 local FontManager = require("instances/FontManager")
 local TestScene = Class:new()
 
-local font = FontManager.get("Modak.ttf", 20)
-local keyBtn = "";
-local mouseBtn = "";
+local font = FontManager.get("Helvetica.ttf", 22)
+local screenHeight = love.graphics.getHeight()
+
+local callbacks = {
+	gamepadaxis = {
+        joystick = "",
+        axis = "",
+        value = "",
+    },
+	gamepadpressed = {
+        joystick = "",
+        button = "",
+    },
+    gamepadreleased = {
+        joystick = "",
+        button = "",
+    },
+	joystickadded = {
+        joystick = "",
+    },
+	joystickaxis = {
+        joystick = "",
+        axis = "",
+        value = "",
+    },
+	joystickhat = {
+        joystick = "",
+        hat = "",
+        direction = "",
+    },
+	joystickpressed = {
+        joystick = "",
+        button = "",
+    },
+	joystickreleased = {
+        joystick = "",
+        button = "",
+    },
+	joystickremoved = {
+        joystick = "",
+    },
+	keypressed = {
+        key = "",
+        scancode = "",
+        isrepeat = "",
+    },
+	keyreleased = {
+        key = "",
+        scancode = "",
+    },
+	mousefocus = {
+        focus = "",
+    },
+	mousemoved = {
+        x = "",
+        y = "",
+        dx = "",
+        dy = "",
+        istouch = "",
+    },
+    mousepressed = {
+        x = "",
+        y = "",
+        button = "",
+        istouch = "",
+        pressed = "",
+    },
+	mousereleased = {
+        x = "",
+        y = "",
+        button = "",
+        istouch = "",
+        presses = "",
+    },
+	textinput = {
+        text = "",
+    },
+	touchmoved = {
+        id = "",
+        x = "",
+        y = "",
+        dx = "",
+        dy = "",
+        pressure = "",
+    },
+    touchpressed = {
+        id = "",
+        x = "",
+        y = "",
+        dx = "",
+        dy = "",
+        pressure = "",
+    },
+	touchreleased = {
+        id = "",
+        x = "",
+        y = "",
+        dx = "",
+        dy = "",
+        pressure = "",
+    },
+	wheelmoved = {
+        x = "",
+        y = "",
+    },
+}
 
 TestScene.draw = function()
     love.graphics.setFont(font)
 
-    love.graphics.print("key: " .. keyBtn, 30, 20)
-    love.graphics.print("mouse: " .. mouseBtn, 30, 40)
+    local x = 30
+    local y = 20
+    for callback in pairs(callbacks) do
+        love.graphics.print(callback, x, y)
+
+        for key, value in pairs(callbacks[callback]) do
+            y = y + 25
+            love.graphics.print(key .. ": " .. value, x + 15, y)
+        end
+
+        if y > screenHeight - 200 then
+            x = x + 400
+            y = 20
+        else
+            y = y + 45
+        end
+    end
 end
 
-TestScene.keypressed = function(key, scancode, isrepeat)
-    keyBtn = key
+TestScene.resize = function(w, h)
+    screenHeight = love.graphics.getHeight()
 end
 
-TestScene.mousepressed = function(x, y, button, istouch, presses)
-    mouseBtn = tostring(button)
+for callback in pairs(callbacks) do
+    TestScene[callback] = function(...)
+        local args = {...}
+        local i = 1
+
+        for key in pairs(callbacks[callback]) do
+            callbacks[callback][key] = tostring(args[i])
+            i = i + 1
+        end
+    end
 end
 
 return TestScene
