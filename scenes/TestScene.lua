085f9bb15c6f6b5b36a74355178a6c76a8c9b9e9
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2020-09-17 18:02:21 +0200

    display table key

--- scenes/TestScene.lua
+++ scenes/TestScene.lua
@@ -13,25 +13,28 @@ function printT(tableToPrint)
     local tables = {{tableToPrint,"  "}}
     local oldTables = {}
     local tablesLength
+    local t
     local tTabs
     local str = ""
     local loopCount = 0
     while next(tables) ~= nil do
         tablesLength = #tables
-        tTabs = tables[tablesLength][2]
-        str = str .. format("%s%s\n", tTabs, tostring(tables[tablesLength][1]))
-        for key, value in pairs(tables[tablesLength][1]) do
+        t = tables[tablesLength]
+        remove(tables, tablesLength)
+        tTabs = t[2]
+        str = str .. format("%s%s  =  %s\n", tTabs,tostring(t[3]), tostring(t[1]))
+        for key, value in pairs(t[1]) do
             if type(value) ~= "table" then
                 str = str .. format("%s  %s  =  %s\n", tTabs, tostring(key), tostring(value))
             elseif oldTables[tostring(value)] == nil then
                 tables[#tables+1] = {
                     value,
-                    tTabs .. "  "
+                    tTabs .. "  ",
+                    key
                 }
                 oldTables[tostring(value)] = true
             end
         end
-        remove(tables, tablesLength)
         loopCount = loopCount + 1
         if loopCount == 50 then
             print(str)
