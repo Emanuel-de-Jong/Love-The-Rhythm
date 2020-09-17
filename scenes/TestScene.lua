b32f76c12974a5d1c7ca9f27ca60363bcc79d37b
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2020-09-17 17:39:49 +0200

    init

--- scenes/TestScene.lua
+++ scenes/TestScene.lua
@@ -2,175 +2,39 @@
 testing stuff.
 --]]
 
-local FontManager = require("instances/FontManager")
 local TestScene = Class:new()
 
-local font = FontManager.get("Helvetica.ttf", 22)
-local screenHeight = love.graphics.getHeight()
-
-local callbacks = {
-	"keypressed",
-	"keyreleased",
-	"mousefocus",
-	"mousemoved",
-	"mousepressed",
-	"mousereleased",
-	"wheelmoved",
-	"gamepadaxis",
-	"gamepadpressed",
-	"gamepadreleased",
-	"joystickadded",
-	"joystickremoved",
-	"joystickaxis",
-	"joystickhat",
-	"joystickpressed",
-	"joystickreleased",
-	"touchmoved",
-	"touchpressed",
-	"touchreleased",
-	"textinput",
-}
-
-local values = {
-	gamepadaxis = {
-        joystick = "",
-        axis = "",
-        value = "",
-    },
-	gamepadpressed = {
-        joystick = "",
-        button = "",
-    },
-    gamepadreleased = {
-        joystick = "",
-        button = "",
-    },
-	joystickadded = {
-        joystick = "",
-    },
-	joystickaxis = {
-        joystick = "",
-        axis = "",
-        value = "",
-    },
-	joystickhat = {
-        joystick = "",
-        hat = "",
-        direction = "",
-    },
-	joystickpressed = {
-        joystick = "",
-        button = "",
-    },
-	joystickreleased = {
-        joystick = "",
-        button = "",
-    },
-	joystickremoved = {
-        joystick = "",
-    },
-	keypressed = {
-        key = "",
-        scancode = "",
-        isrepeat = "",
-    },
-	keyreleased = {
-        key = "",
-        scancode = "",
-    },
-	mousefocus = {
-        focus = "",
-    },
-	mousemoved = {
-        x = "",
-        y = "",
-        dx = "",
-        dy = "",
-        istouch = "",
-    },
-    mousepressed = {
-        x = "",
-        y = "",
-        button = "",
-        istouch = "",
-        pressed = "",
-    },
-	mousereleased = {
-        x = "",
-        y = "",
-        button = "",
-        istouch = "",
-        presses = "",
-    },
-	textinput = {
-        text = "",
-    },
-	touchmoved = {
-        id = "",
-        x = "",
-        y = "",
-        dx = "",
-        dy = "",
-        pressure = "",
-    },
-    touchpressed = {
-        id = "",
-        x = "",
-        y = "",
-        dx = "",
-        dy = "",
-        pressure = "",
-    },
-	touchreleased = {
-        id = "",
-        x = "",
-        y = "",
-        dx = "",
-        dy = "",
-        pressure = "",
-    },
-	wheelmoved = {
-        x = "",
-        y = "",
-    },
-}
-
-TestScene.draw = function()
-    love.graphics.setFont(font)
-
-    local x = 30
-    local y = 20
-    for _, callback in pairs(callbacks) do
-        love.graphics.print(callback, x, y)
-
-        for key, value in pairs(values[callback]) do
-            y = y + 25
-            love.graphics.print(key .. ": " .. value, x + 15, y)
+function printT(tableToPrint)
+    local next, pairs, type, format, tostring, remove, print = next, pairs, type, string.format, tostring, table.remove, print
+    local tables = {{tableToPrint,"  "}}
+    local oldTables = {}
+    local tablesLength
+    local tTabs
+    local str = ""
+    local loopCount = 0
+    while next(tables) ~= nil do
+        tablesLength = #tables
+        tTabs = tables[tablesLength][2]
+        for key, value in pairs(tables[tablesLength][1]) do
+            if type(value) ~= "table" then
+                str = str .. format("%s%s  =  %s\n", tTabs, tostring(key), tostring(value))
+            elseif oldTables[tostring(value)] == nil then
+                tables[#tables+1] = {
+                    value,
+                    tTabs .. "  "
+                }
+                oldTables[tostring(value)] = true
+            end
         end
-
-        if y > screenHeight - 200 then
-            x = x + 400
-            y = 20
-        else
-            y = y + 45
+        remove(tables, tablesLength)
+        loopCount = loopCount + 1
+        if loopCount == 50 then
+            print(str)
+            str = ""
+            loopCount = 0
         end
     end
-end
-
-TestScene.resize = function(w, h)
-    screenHeight = love.graphics.getHeight()
-end
-
-for callback in pairs(values) do
-    TestScene[callback] = function(...)
-        local args = {...}
-        local i = 1
-
-        for key in pairs(values[callback]) do
-            values[callback][key] = tostring(args[i])
-            i = i + 1
-        end
-    end
+    print(str)
 end
 
 return TestScene
